9f3fabd05195ff0eabbe6e160b6ab21e8542753f
Alessandro Badiglio <a.badiglio@pindarcreative.co.uk>
2021-06-06 21:46:20 +0100

    Add method and Test for Weather class

--- app/Classes/Weather.php
+++ app/Classes/Weather.php
@@ -15,4 +15,29 @@ class Weather
         $this->weatherApi = $weatherApi;
         $this->city = $city;
     }
+
+    public function getWeather()
+    {
+        $this->weatherApi->setCity($this->city);
+        $this->weather_data = $this->weatherApi->getweather();
+
+        $response = [];
+
+        if (!empty($this->weather_data)) {
+            $response['city'] = $this->city;
+            $response['today_forecast'] = $this->getForecast($this->weather_data, 0);
+            $response['tomorrow_forecast'] = $this->getForecast($this->weather_data, 1);
+        }
+
+        return $response;
+    }
+
+    public function getForecast($response, $day)
+    {
+        if (isset($response->forecast->forecastday[$day]->day->condition->text)) {
+            return $response->forecast->forecastday[$day]->day->condition->text;
+        }
+
+        return "";
+    }
 }
--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -23,4 +23,53 @@ class WeatherTest extends TestCase
 
         $this->assertInstanceOf('App\Classes\Weather', $weather);
     }
+
+    public function testGetWeatherWithRealCity()
+    {
+        $weather = new Weather(self::$weatherApi, 'Palermo');
+
+        $this->assertIsArray($weather->getWeather('Palermo'));
+    }
+
+    public function testGetWeatherWithFakeCity()
+    {
+        $weather = new Weather(self::$weatherApi, 'asasdadd');
+
+        $resp = $weather->getWeather();
+
+        $this->assertEmpty($resp);
+    }
+
+    public function testGetWeatherWithEmptyCity()
+    {
+        $weather = new Weather(self::$weatherApi, '');
+
+        $resp = $weather->getWeather('');
+
+        $this->assertEmpty($resp);
+    }
+
+    public function testgetForecastWithRealCity()
+    {
+        $weather = new Weather(self::$weatherApi, 'Palermo');
+        $resp = $weather->getWeather();
+
+        $today_forecast = $weather->getForecast($resp, 0);
+        $this->assertIsString('string', $today_forecast);
+
+        $tomorrow_forecast = $weather->getForecast($resp, 1);
+        $this->assertIsString('string', $tomorrow_forecast);
+    }
+
+    public function testgetForecastWithFakeCity()
+    {
+        $weather = new Weather(self::$weatherApi, 'asasdadd');
+        $resp = $weather->getWeather();
+
+        $today_forecast = $weather->getForecast($resp, 0);
+        $this->assertEmpty($today_forecast);
+
+        $tomorrow_forecast = $weather->getForecast($resp, 1);
+        $this->assertEmpty($tomorrow_forecast);
+    }
 }
