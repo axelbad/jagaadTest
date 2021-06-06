b0a9becf29adbb43283c54ea02d656b836e058eb
Alessandro Badiglio <a.badiglio@pindarcreative.co.uk>
2021-06-06 21:28:55 +0100

    Add getCities method to Musement class

--- app/Classes/Musement.php
+++ app/Classes/Musement.php
@@ -9,4 +9,27 @@ class Musement
 {
     private $cities;
     private $cities_name = [];
+
+    public function getCities()
+    {
+        $curl = new Curl();
+
+        try {
+            $curl->get('https://sandbox.musement.com/api/v3/cities');
+
+            $response = $curl->response;
+
+            if ((!key_exists('code', $response)) && ($curl->getHttpStatusCode() == 200)) {
+                $this->cities = $curl->response;
+
+                foreach ($this->cities as $city) {
+                    $this->cities_name[] = $city->name;
+                }
+            }
+
+            return $this->cities_name;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
 }
