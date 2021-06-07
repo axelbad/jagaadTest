1e05925f77a66d7906c067f638c4a052442ed743
Alessandro Badiglio <a.badiglio@pindarcreative.co.uk>
2021-06-07 12:03:28 +0100

    Refactoring init/index

--- app/Classes/Init.php
+++ app/Classes/Init.php
@@ -13,11 +13,39 @@ use Monolog\Logger;
  *
  * @author    Alessandro Badiglio <axelba@gmail.com>
  */
+
 class Init
 {
+    /**
+     * The Musement API object.
+     *
+     * @var object
+     */
+
     private $musement;
+
+    /**
+     * The WeatherApi API object.
+     *
+     * @var object
+     */
+
     private $weatherApi;
+
+    /**
+     * The Log object used through the app: default (Monolog)
+     *
+     * @var object
+     */
+
     private $logger;
+
+    /**
+     * The string used as end of line
+     *
+     * @var string
+     */
+
     private $eol = '<br>';
 
     public function __construct()
@@ -30,11 +58,18 @@ class Init
         $this->setEol();
     }
 
+    /**
+     * After retrieved the cities and their forecast
+     * return just a string if cli is detected
+     * otherwise apply the scaffolding to be used as normal html page
+     *
+     * @return string
+     */
+
     public function getCityForecast()
     {
         $cities = $this->musement->getCities();
         $forecast_city = [];
-        $i = 0;
 
         $cities_forecast = "";
         foreach ($cities as $city) {
@@ -48,11 +83,16 @@ class Init
             }
         }
 
+        // Using the html template if cli is detected
+        if (!$this->isCli()) {
+            $cities_forecast = str_replace('{cities_forecast}', $cities_forecast, $this->scaffolding());
+        }
+
         return $cities_forecast;
     }
 
     // Detect if php is running from cli
-    public function isCli()
+    private function isCli()
     {
         if (empty($_SERVER['REMOTE_ADDR']) and !isset($_SERVER['HTTP_USER_AGENT']) and count($_SERVER['argv']) > 0) {
             return true;
@@ -61,10 +101,33 @@ class Init
         return false;
     }
 
-    public function setEol()
+    // If cli is detected set the end of file
+    private function setEol()
     {
         if ($this->isCli()) {
             $this->eol = PHP_EOL;
         }
+    }
+
+    // Html template that wiil be used if the app is called within a browser
+    private function scaffolding()
+    {
+        return <<<MUC
+        <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jagaad Test</title>
+</head>
+
+<body>
+    {cities_forecast}
+</body>
+
+</html>
+MUC;
     }
 }
--- index.php
+++ index.php
@@ -7,27 +7,4 @@ use App\Classes\Init;
 $init = new Init();
 $cities_forecast = $init->getCityForecast();
 
-$isCli = false;
-if ($init->isCli()) {
-    echo $cities_forecast;
-    die();
-}
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jagaad Test</title>
-</head>
-
-<body>
-<?php
-    echo $cities_forecast;
-?>
-</body>
-
-</html>
+echo $cities_forecast;
